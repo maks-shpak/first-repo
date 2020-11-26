e89841365fa90ca086fbff1abeaad0ad024c03d1
Maks Shpak <maksimshpak20047a@gmail.com>
2020-11-26 15:24:57 +0200

    Create Нок Нод( python) (2).py
    add file

---
+++ Нок Нод( python) (2).py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+def NOD(a, b):
+	for x in range(min(a, b)):
+		if a%(min(a, b)-x) == b%(min(a, b)-x) == 0
+		return min(a, b)-x
+def NOK(a, b, c):
+	return(a*b/c)
+c = NOD(a, b)
+z = NOK(a, b, c)
+print(c, int(z))
